86f44f4550f50f3440961414d474af6ab8ab8574
lordiwe <39370536+lordiwe@users.noreply.github.com>
2020-01-31 22:57:04 +0200

    Updated MaterialChanger script

---
+++ Assets/OldStaffToChange/MaterialChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    public Material texture;
+    //public int CurrentTexture;
+    public bool changed = false;
+    public Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        //rend.enabled = true;
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!changed)
+            {
+                changed = true;
+                other.GetComponent<Renderer>().material = texture;
+                //CurrentTexture++;
+                //CurrentTexture %= textures.Length;
+                //GetComponent<Renderer>().material = textures[CurrentTexture];
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            changed = false;
+        }
+    }
+
+}
